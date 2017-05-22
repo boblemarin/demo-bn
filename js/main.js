0dd5027cf206996f8f1c61bc939d866f12981ac0
Lionel <lionel@pierre-papier-pixel.com>
2017-05-22 10:20:54 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function (){
+    $("#erasable-canvas").attr('width', $("#erasable-canvas").width());
+    $("#erasable-canvas").attr('height', $("#erasable-canvas").height());
+    var canvas = $("#erasable-canvas");
+    var context = canvas.get(0).getContext("2d");
+    console.log('jquery ok');
+    var imgObj = new Image();
+
+    imgObj.onload = function() {
+        console.log('img chargée');
+        context.drawImage(imgObj, 0, 0, canvas.width(), canvas.height());
+    };
+    imgObj.src = "../img/erasable.jpg";
+});
